f801a72946907d522ff10414a78c290648f2678a
Manoj <manojkumarpanigrahi54@gmail.com>
2026-01-09 19:03:43 +0530

    type converion

---
+++ DAY1/typeconversion.java
@@ -0,0 +1,8 @@
+public class typeconversion {
+    public static void main(String [] args){
+        float f = 5.6f;
+        int i = (int)f;
+        System.out.println(i);
+        System.out.println(f);
+    }
+}
