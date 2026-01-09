05b83d7230ed3c5d53a0a34a7c0fdff58cb3ab31
Manoj <manojkumarpanigrahi54@gmail.com>
2026-01-09 20:25:16 +0530

    logical operator

--- DAY1/operator.java
+++ DAY1/operator.java
@@ -9,5 +9,12 @@ public class operator {
         boolean result1 = x == y;
         System.out.println(result1);
 
+        int p = 5;
+        int q = 6;
+        int r = 7;
+        int s = 8;
+        boolean result2 = (p > q) || (r < s);
+        System.out.println(result2);
+
     }
 }
