a9fa284b7ed433e666643fff4d801650d341da84
Manoj <manojkumarpanigrahi54@gmail.com>
2026-01-10 11:42:30 +0530

    update

--- DAY2/newss.java
+++ DAY2/newss.java
@@ -1,6 +1,7 @@
 public class newss {
     public static void main(String[] args){
         String day ="sunday";
+        String result = "";
 
         switch(day){
             case "monday","tuesday" -> System.out.println("6 am");
