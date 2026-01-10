fbcabf8b70eeb6ff9af2ca4da0c42d618d3ae253
Manoj <manojkumarpanigrahi54@gmail.com>
2026-01-10 10:32:08 +0530

    if else

---
+++ DAY2/ifelse.java
@@ -0,0 +1,14 @@
+
+
+public class ifelse {
+    
+    public static void main(String[] args) {
+        int x = 20;
+        int y = 30;
+        if(x>y){
+            System.out.println(x);
+        }else{
+            System.out.println(y);
+        }
+    }
+}
