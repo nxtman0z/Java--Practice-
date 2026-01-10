527123cde44c3fedbb251d6175b7727a2af38f91
Manoj <manojkumarpanigrahi54@gmail.com>
2026-01-10 11:06:26 +0530

    ternary operator

---
+++ DAY2/ternary.java
@@ -0,0 +1,10 @@
+
+public class ternary {
+
+    public static void main (String[] args)
+{
+        int x = 5;
+
+        String result = (x % 2 == 0) ? "even" : "odd";
+        System.out.println(result);
+}}
