d9b299cff20eb086e323b57c2f5861947fe70bf8
Manoj <manojkumarpanigrahi54@gmail.com>
2026-01-12 07:16:53 +0530

    oopsconcept

---
+++ DAY3/objects.java
@@ -0,0 +1,10 @@
+package DAY3;
+
+public class objects {
+    public static void main(String[] args){
+        int a =8;
+        int b = 10;
+        int sum = a+b;
+        System.out.println(sum);
+    }
+}
