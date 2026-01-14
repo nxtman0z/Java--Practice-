9ee1234f17babeb5efc7dfa4173b56928a5636a0
Manoj <manojkumarpanigrahi54@gmail.com>
2026-01-14 20:12:54 +0530

    arrays

--- DAY4/array.java
+++ DAY4/array.java
@@ -1,6 +1,7 @@
 public class array {
     public static void main(String[] args) {
         int nums[] ={4,6,8,3};
-        System.out.println(nums[0]);
+        nums[1] = 8;
+        System.out.println(nums[1]);
     }
 }
