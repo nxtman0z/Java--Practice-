0d8728cba14362f0fe6a9dddf7e7b2f3ebd0d280
Manoj <manojkumarpanigrahi54@gmail.com>
2026-01-14 20:17:13 +0530

    arrays

--- DAY4/array.java
+++ DAY4/array.java
@@ -3,5 +3,16 @@ public class array {
         int nums[] ={4,6,8,3};
         nums[1] = 8;
         System.out.println(nums[1]);
+
+        //define array length
+        int num[] =new int[4];
+        num[0]=1;
+        num[1]=7;
+        num[2]=87;
+        num[3]=9;
+        for(int i =0;i<num.length;i++){
+            System.out.println(num[i]);
+        }
+
     }
 }
