1ee7fe545525404390bd4fc60c59cd972332e65f
Manoj <manojkumarpanigrahi54@gmail.com>
2026-01-16 21:00:58 +0530

    array of objects

---
+++ DAY5/arrobjects.java
@@ -0,0 +1,15 @@
+public class arrobjects {
+    public static void main(String[] args) {
+        int arr[] = new int[6];
+        arr[0] = 7;
+        arr[1] = 4;
+        arr[2] = 5;
+        arr[3] = 8;
+
+        for(int i =0;i<6;i++){
+            System.out.println(arr[i]);
+        }
+
+
+    }
+}
