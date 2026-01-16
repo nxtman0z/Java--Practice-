e0a40c592abf977ded9f334b1d8c3eafee1a1587
Manoj <manojkumarpanigrahi54@gmail.com>
2026-01-16 20:22:45 +0530

    multi dimensional array

--- DAY5/multiarray.java
+++ DAY5/multiarray.java
@@ -4,8 +4,16 @@ public class multiarray {
 
         for (int i =0;i <3;i++){
             for(int j =0;j<4;j++){
+                num[i][j] = (int)(Math.random()*100);
                 System.out.println(num[i][j]);
             }
+        }
+
+        for (int i =0;i <3;i++){
+            for(int j =0;j<4;j++){
+                System.out.print(num[i][j] +" ");
+            }
+            System.out.println();
         }
     }
 }
