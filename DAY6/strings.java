1eb2624f46247ca379cbfffc6f0a503ccd07138d
Manoj <manojkumarpanigrahi54@gmail.com>
2026-01-18 19:56:58 +0530

    concatination in java

--- DAY6/strings.java
+++ DAY6/strings.java
@@ -4,5 +4,6 @@ public class strings {
     public static void main(String[] args) {
         String name = new String("manoj");
         System.out.println(name);
+        System.out.println(name.concat("obito"));
     }
 }
