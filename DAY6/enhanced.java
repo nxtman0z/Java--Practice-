65aeecde7b93446649ec6e8075da08828fe7dd9c
Manoj <manojkumarpanigrahi54@gmail.com>
2026-01-18 19:26:24 +0530

    enhanced loop

---
+++ DAY6/enhanced.java
@@ -0,0 +1,14 @@
+public class enhanced{
+    public static void main(String[] args){
+        int arr[] = new int[3];
+
+        arr[0] = 5;
+        arr[1] = 3;
+        arr[2] = 8;
+
+
+        for(int n : arr){
+            System.out.println(n);
+        }
+    }
+}
