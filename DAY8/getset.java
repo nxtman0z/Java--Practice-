c5a60ec1c9e20ae9cd7c0cd24b87d348f8300567
Manoj <manojkumarpanigrahi54@gmail.com>
2026-01-19 19:56:55 +0530

    this keyword

--- DAY8/getset.java
+++ DAY8/getset.java
@@ -1,5 +1,31 @@
-package D
+class Human{
+    private int age ;
+    private String name;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
 
 public class getset {
-    
+    public static void main(String[] args) {
+        Human obj = new Human();
+        obj.setAge(30);
+        obj.setName("manoj");
+        System.out.println(obj.getName() + obj.getAge());
+    }
 }
