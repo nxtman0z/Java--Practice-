e9b68f3cbec57f45785369208856713eacd7ddc5
Manoj <manojkumarpanigrahi54@gmail.com>
2026-01-19 12:18:51 +0530

    static block

---
+++ DAY7/staticblock.java
@@ -0,0 +1,37 @@
+
+class Mobile{
+    String brand;
+    int price;
+    static String name;
+
+
+    static
+{
+    name = "phone";
+    System.out.println("static block");
+}
+
+public Mobile(){
+    brand = "";
+    price = 200;
+
+    System.out.println("constructor");
+    }
+
+    public void show(){
+        System.out.println(brand + price + name );
+    }
+}
+
+//static block
+
+
+public class staticblock {
+    public static void main(String[] args) {
+        Mobile m1 = new Mobile();
+        m1.brand = "apple";
+        m1.price = 1000;
+        Mobile.name = "smartphone";
+        Mobile m2 = new Mobile();
+    }
+}
