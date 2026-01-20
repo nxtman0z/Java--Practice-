db5f1978df389c482c2d033fd71b3309222a471a
Manoj <manojkumarpanigrahi54@gmail.com>
2026-01-20 19:43:22 +0530

    anonymous object

---
+++ day9/anonymous.java
@@ -0,0 +1,12 @@
+class A{
+    public A(){
+        System.out.println("constructor called");
+    }
+}
+
+public class anonymous {
+    public static void main(String[] args) {
+        new A();
+    }
+
+}
