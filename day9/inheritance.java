2028efc767330bcfe3caadb1c4360075a51054c7
Manoj <manojkumarpanigrahi54@gmail.com>
2026-01-20 20:38:29 +0530

    inheritance

---
+++ day9/inheritance.java
@@ -0,0 +1,23 @@
+class Cal
+{
+    public int add(int a ,int b){
+        return a + b;
+    }
+    public int sub(int a ,int b){
+        return a - b;
+    }
+    public int mul(int a ,int b){
+        return a * b;
+    }
+
+   
+}
+
+
+
+
+public class inheritance {
+    public static void main(String[] args) {
+        
+    }
+}
