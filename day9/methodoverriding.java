097347c1c481e63e899106d751daa19a8eb020b3
Manoj <manojkumarpanigrahi54@gmail.com>
2026-01-20 22:40:30 +0530

    method overriding

---
+++ day9/methodoverriding.java
@@ -0,0 +1,19 @@
+class Calc{
+    public int add(int a ,int b){
+        return a +b;
+    }
+}
+
+class Advcalc extends Calc{
+
+}
+
+
+public class methodoverriding {
+    public static void main(String[] args) {
+        Advcalc obj = new Advcalc();
+        int n =obj.add(4, 6);
+        System.out.println(n);
+
+    }
+}
