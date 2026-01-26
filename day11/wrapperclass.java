8f207c9424bd801aa1e37cb7d7963394aaa60030
Manoj <manojkumarpanigrahi54@gmail.com>
2026-01-26 21:35:02 +0530

    wrapper class

---
+++ day11/wrapperclass.java
@@ -0,0 +1,11 @@
+public class wrapperclass {
+    public static void main(String[] args) {
+
+        int a = 10;
+
+        Integer obj = a;   
+        int b = obj;       
+        System.out.println(obj);
+        System.out.println(b);
+    }
+}
