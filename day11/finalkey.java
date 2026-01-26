da06e8b75bf0c449eb1d54353896fa318b04efa4
Manoj <manojkumarpanigrahi54@gmail.com>
2026-01-26 20:10:14 +0530

    final variable,method and class

--- day11/finalkey.java
+++ day11/finalkey.java
@@ -1,9 +1,14 @@
+final class Vehicle {   
 
+    final int speedLimit = 80;  
+    final void showSpeedLimit() {  
+        System.out.println("Speed Limit is: " + speedLimit);
+    }
+}
 
 public class finalkey {
     public static void main(String[] args) {
-        final int num =8;
-        //num = 9;
-        System.out.println(num);
+        Vehicle v = new Vehicle();
+        v.showSpeedLimit();
     }
 }
