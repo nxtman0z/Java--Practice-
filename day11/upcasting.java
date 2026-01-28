73f05d4c46394658ab9d8a11f4818fdeac918f76
Manoj <manojkumarpanigrahi54@gmail.com>
2026-01-28 21:27:48 +0530

    upcasting in java

--- day11/upcasting.java
+++ day11/upcasting.java
@@ -2,6 +2,10 @@ class Animal{
     void sound(){
         System.out.println("Animal sounds");
     }
+
+    void fetch() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 }
 class Dog extends Animal{
     void sound(){
@@ -16,5 +20,6 @@ public class upcasting {
     public static void main(String[] args) {
         Animal a = new Dog();
         a.sound();
+        a.fetch();
     }
 }
