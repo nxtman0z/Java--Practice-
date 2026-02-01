70dd29c82679093195866ed1637a45963a94d4d6
Manoj <manojkumarpanigrahi54@gmail.com>
2026-02-01 20:34:51 +0530

    enum class in java

--- day13/enumclass.java
+++ day13/enumclass.java
@@ -1,5 +1,21 @@
-package day13;
+enum Laptop{
+    Mackbook(2000),Xps(2200),Surface(1500),Thinkpad(2400);
+    private int price;
+    private Laptop(int price){
+        this.price = price;
+    }
+    public int getPrice(){
+        return price;
+    }
+    public int setPrice(){
+        this.price = price;
+        return 0;
+    }
+}
 
 public class enumclass {
-    
+    public static void main(String[] args) {
+        Laptop lap = Laptop.Mackbook;
+        System.out.println(lap + ":" + lap.getPrice());
+    }
 }
