4c5eb01b916312e75fcf4c8ec626c8145cfe4fd6
Manoj <manojkumarpanigrahi54@gmail.com>
2026-02-02 19:20:13 +0530

    annotation in java

--- day14/annotation.java
+++ day14/annotation.java
@@ -2,19 +2,20 @@
 
 
 class A{
-    public void show(){
+    public void showMeAtTheCornerLikeYouThe(){
         System.out.println("in a show");
     }
 }
 
 class B extends A{
-    public void show(){
+    @Override
+    public void showMeAtTheCornerLikeYouThe(){
         System.out.println("in b show");
     }
 }
 public class annotation {
     public static void main(String[] args) {
         B obj = new B();
-        obj.show();
+        obj.showMeAtTheCornerLikeYouThe();
     }
 }
