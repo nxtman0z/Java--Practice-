9e9c299a58f1c525fde3df6c3860f0bffee61f6c
Manoj <manojkumarpanigrahi54@gmail.com>
2026-02-03 19:09:30 +0530

    lambada expression with return

--- day15/lambda.java
+++ day15/lambda.java
@@ -1,16 +1,16 @@
 @FunctionalInterface
 interface A{
-    void show(int i);
+    int add(int i , int j);
 }
 
 public class lambda {
     public static void main(String[] args) {
-        A obj =  i ->
-        
-            
-                System.out.println("in show0" );
-            
-        
-        obj.show(5);
+        A obj = new A(){
+            public int add(int i ,int j){
+                return i+j;
+            }
+        };
+        int result=obj.add(5,6);
+        System.out.println(result);
     }
 }
