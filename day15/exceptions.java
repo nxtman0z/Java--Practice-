0bd26305d57ee8cd1e1534694e30fbd37ec574ae
Manoj <manojkumarpanigrahi54@gmail.com>
2026-02-03 19:35:59 +0530

    try catch block in java

--- day15/exceptions.java
+++ day15/exceptions.java
@@ -3,7 +3,12 @@
 public class exceptions {
     public static void main(String[] args) {
         int i =0;
-        int j = 18/i;
+        int j = 0;
+        try {
+            j = 18/i;
+        } catch (Exception e) {
+            System.out.println("something went wrong");
+        }
         System.out.println(j);
     }
 }
