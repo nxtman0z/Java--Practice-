70ca2f4a17397ba2b000c5dcd88374ee5fe23acd
Manoj <manojkumarpanigrahi54@gmail.com>
2026-02-03 19:12:06 +0530

    lamba expression with return statement

--- day15/lambda.java
+++ day15/lambda.java
@@ -5,11 +5,7 @@ interface A{
 
 public class lambda {
     public static void main(String[] args) {
-        A obj = new A(){
-            public int add(int i ,int j){
-                return i+j;
-            }
-        };
+        A obj = (i,j) -> i+j;
         int result=obj.add(5,6);
         System.out.println(result);
     }
