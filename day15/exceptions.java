32f3ac6ab5eebe1521a510dbc6e6526ea2f83b50
Manoj <manojkumarpanigrahi54@gmail.com>
2026-02-03 19:58:28 +0530

    multiple try catch in java

--- day15/exceptions.java
+++ day15/exceptions.java
@@ -2,12 +2,21 @@
 
 public class exceptions {
     public static void main(String[] args) {
-        int i =7;
-        int j = 0;
+        int i =0;
+        int j =0;
+
+        int nums[] = new int[5];
+
         try {
             j = 18/i;
-        } catch (Exception e) {
-            System.out.println("something went wrong");
+            System.out.println(nums[1]);
+            System.out.println(nums[5]);
+
+        } catch (ArithmeticException e) {
+            System.out.println("cannot divide by zero");
+        }
+        catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("stay in your range");
         }
         System.out.println(j);
     }
