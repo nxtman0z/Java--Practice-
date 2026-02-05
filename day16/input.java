004158a47aefc1d9b9d7bce3cd262fdec1bafd2b
Manoj <manojkumarpanigrahi54@gmail.com>
2026-02-05 19:20:04 +0530

    input in java

---
+++ day16/input.java
@@ -0,0 +1,9 @@
+
+import java.io.IOException;
+public class input {
+    public static void main(String[] args) throws IOException {
+        System.out.println("enter a number:");
+        int num = System.in.read();
+        System.out.println(num);
+    }
+}
