6366fcc940ab5e852466662eb9c905dd19746dd3
Manoj <manojkumarpanigrahi54@gmail.com>
2026-02-05 19:43:01 +0530

    final keyword

---
+++ day16/trywithresources.java
@@ -0,0 +1,18 @@
+
+
+public class trywithresources {
+    public static void main(String[] args) {
+
+        int i =0;
+        int j =0;
+        try {
+            j = 18/i;
+        } 
+        catch(Exception e){
+            System.out.println("get lost");
+        }
+        
+        finally {
+        }
+    }
+}
