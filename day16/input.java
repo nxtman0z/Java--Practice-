a80b899d280c769ea190cd86f2291a5ae24ba178
Manoj <manojkumarpanigrahi54@gmail.com>
2026-02-05 19:29:05 +0530

    bufferreader input

--- day16/input.java
+++ day16/input.java
@@ -1,9 +1,17 @@
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 public class input {
     public static void main(String[] args) throws IOException {
         System.out.println("enter a number:");
-        int num = System.in.read();
-        System.out.println(num - 48);
+        InputStreamReader in = new InputStreamReader(System.in);
+
+        BufferedReader bf = new BufferedReader(in);
+        
+
+        int num = Integer.parseInt(bf.readLine());
+        System.out.println(num);
+
     }
 }
