1ea6eb5783eaf33673ecf1f9c99881d8eb561b4f
Manoj <manojkumarpanigrahi54@gmail.com>
2026-02-05 19:21:02 +0530

    input in java

--- day16/input.java
+++ day16/input.java
@@ -4,6 +4,6 @@ public class input {
     public static void main(String[] args) throws IOException {
         System.out.println("enter a number:");
         int num = System.in.read();
-        System.out.println(num);
+        System.out.println(num - 48);
     }
 }
