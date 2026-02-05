36c8cefe89bc71f92782263f00596c3b30764fe1
Manoj <manojkumarpanigrahi54@gmail.com>
2026-02-05 20:10:54 +0530

    try with resources in java

--- day16/trywithresources.java
+++ day16/trywithresources.java
@@ -1,17 +1,20 @@
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
 
 public class trywithresources {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        int i =0;
-        int j =0;
-        try {
-            j = 18/i;
+        int num = 0;
+       
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            num = Integer.parseInt(br.readLine());
+            System.out.println(num);
         } 
-        // catch(Exception e){
-        //     System.out.println("get lost");
-        // }
-        
+    
         finally {
             System.out.println("bye");
         }
