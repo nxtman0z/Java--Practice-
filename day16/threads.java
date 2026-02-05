ebe3ff2734c5825f6544794a65b4280f957e6a60
Manoj <manojkumarpanigrahi54@gmail.com>
2026-02-05 20:40:32 +0530

    threads in java

---
+++ day16/threads.java
@@ -0,0 +1,26 @@
+
+class A{
+    public void show(){
+        for(int i =1;i<=10;i++){
+                System.out.println("hi");
+        }
+        
+    }
+}
+
+class B{
+    public void show(){
+        for(int i =1;i<=10;i++){
+                System.out.println("hello");
+        }
+    }
+}
+public class threads {
+    public static void main(String[] args) {
+        A obj1 = new A();
+        B obj2 = new B();
+
+        obj1.show();
+        obj2.show();
+    }
+}
