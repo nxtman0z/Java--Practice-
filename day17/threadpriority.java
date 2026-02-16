33027880e1dd89a3358aff3b5a74853b136c518f
Manoj <manojkumarpanigrahi54@gmail.com>
2026-02-16 19:33:38 +0530

    prority thread

--- day17/threadpriority.java
+++ day17/threadpriority.java
@@ -1,3 +1,39 @@
+class A extends Thread{
+    public void run(){
+        for(int i =1;i<=100;i++){
+            System.out.println("hi");
+            try {
+            
+                Thread.sleep(10);
+            } catch (InterruptedException ex) {
+                System.getLogger(A.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
+            }
+        }
+    }
+}
+
+class B extends Thread{
+    public void run(){
+        for(int i =1;i<=100;i++){
+            System.out.println("hello");
+            try {
+            
+                Thread.sleep(10);
+            } catch (InterruptedException ex) {
+                System.getLogger(A.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
+            }
+        }
+    }
+}
+
+
 public class threadpriority {
-    
+    public static void main(String[] args) {
+        A obj1 = new A();
+        B obj2 = new B();
+
+        obj1.run();
+        obj2.run();
+
+    }
 }
